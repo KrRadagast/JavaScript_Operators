6ae04e00227b91ff3449ca488149a8e45e4a7417
Nicholas Poehlman <nicholas.poehlman343@gmail.com>
2023-03-16 08:26:20 -0700

    completed the excesise

--- app.js
+++ app.js
@@ -4,16 +4,56 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+var a=20;
+var b=4;
+var add=a+b;
+var minus=a-b;
+var multiply=a*b;
+var dividing=a/b;
 
+
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
-
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 // YOUR CODE HERE
+add=num+str;
+console.log(add);
+add=num+str2;
+console.log(add);
+add=num+isPresent;
+console.log(add);
+add=firstName+num;
+console.log(add);
+add=isPresent+str;
+console.log(add);
+add=firstName+lastName;
+console.log(add);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
-
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
 // YOUR CODE HERE
+;
+console.log(val==str3);
+console.log(val===str3);
+console.log(!isAwake);
+console.log(("eleven" == str4 && val >= str3));
+console.log(!isAwake || isAwake)
+console.log(0==false)
+console.log(0===false)
+console.log(0!=false)
+console.log(0!==false)
